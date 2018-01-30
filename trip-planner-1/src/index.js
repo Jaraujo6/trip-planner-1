0f47b759ea503d1386a6461e571666fe3d308ee6
Domnik Mack <domnikmack@gmail.com>
2018-01-30 13:17:48 -0500

    markerFac

--- trip-planner-1/src/index.js
+++ trip-planner-1/src/index.js
@@ -1,1 +1,15 @@
-console.log('domnik is the man');
+const mapboxgl = require("mapbox-gl");
+const markerFactory = require('./marker');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG9tbmlrIiwiYSI6ImNqZDF4YmFraDFrZmkycHA0eWl1cWt4b3AifQ.5GxXUApS0xDeOAryWPXhqA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+
+markerFactory('hotel', [-74.009, 40.705]).addTo(map);
